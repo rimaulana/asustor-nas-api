5ad7f9f3bf05bbdb0569e9543436def7e3bef4b7
rimaulana <rimaulana@outlook.com>
2018-06-09 22:30:39 +0700

    removed unnecessary logic

--- controllers/file.js
+++ controllers/file.js
@@ -5,13 +5,9 @@ const asustor = require('@rimaulana/asustor-node');
 const router = express.Router();
 
 const getFile = (req, res) => {
-  if (req.params.filename) {
-    asustor.fileInfo(req.params.filename, (error, data) => {
-      utils.genericReply(res, error, data);
-    });
-  } else {
-    utils.genericReply(res, new Error('filename needs to be specified'), null, 400);
-  }
+  asustor.fileInfo(req.params.filename, (error, data) => {
+    utils.genericReply(res, error, data);
+  });
 };
 
 router.get('/:filename', getFile);
