c23a86adf51eeacd13fa0a5352f1f0658aa0bb3b
rimaulana <rimaulana@outlook.com>
2018-06-24 00:55:06 +0700

    refactored

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,18 +4,19 @@ const config = require('../config');
 
 const router = express.Router();
 
+const warning = (req, res, fault) => {
+  const message = (fault === 'invalid' ? 'with invalid' : 'without');
+  utils.logger.warn(`${utils.cleanIPAddress(req.ip)} - called ${req.url} ${message} access key`);
+  res.status(401).json({ reason: `access token ${fault}` });
+};
+
 router.use((req, res, next) => {
-  const token = req.headers['x-access-token'] || req.query.token;
-  if (token) {
-    if (config.api_keys.indexOf(token) !== -1) {
-      next();
-    } else {
-      utils.logger.warn(`${utils.cleanIPAddress(req.ip)} - called ${req.url} with invalid access key`);
-      res.status(401).json({ reason: 'access token invalid' });
-    }
+  const token = req.headers['x-access-token'] || req.query.token || null;
+  const fault = (token === null ? 'required' : 'invalid');
+  if (fault !== 'required' && config.api_keys.indexOf(token) !== -1) {
+    next();
   } else {
-    utils.logger.warn(`${utils.cleanIPAddress(req.ip)} - called ${req.url} without access key`);
-    res.status(401).json({ reason: 'access token required' });
+    warning(req, res, fault);
   }
 });
 
